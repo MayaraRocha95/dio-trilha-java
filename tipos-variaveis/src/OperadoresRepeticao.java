99cb1de98da3a3d0175b2f1c84eb784c09d3cac0
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-03-02 19:19:07 -0300

    operador repetição

---
+++ tipos-variaveis/src/OperadoresRepeticao.java
@@ -0,0 +1,21 @@
+public class OperadoresRepeticao {
+    
+
+    public static void main(String[] args){
+        int numero = 5;
+        
+
+         //numero ++;
+
+        System.out.println(++ numero);
+
+        System.out.println(numero);
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+    }
+}
