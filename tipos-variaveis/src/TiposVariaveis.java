2a9921a982cb3aaae31c84a75926b9794483d1e7
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-03-02 16:54:49 -0300

    operadores matematicos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
        double salarioMinino = 2500;
+       System.out.print(salarioMinino);
 
        short numeroCurto = 1;
        int numeroNormal = numeroCurto;
        short numeroCurto2 = (short)numeroNormal;
-
+       System.out.print(numeroCurto);
+       
        int numero = 5;
 
        numero = 10;
