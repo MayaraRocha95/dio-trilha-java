3ce266d00aad9dc2bee6a1bbbc5b6a736c4f6c65
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-03-02 16:34:55 -0300

    tipos e variaveis

---
+++ tipos-variaveis/src/TipoDados.java
@@ -0,0 +1,10 @@
+public class TipoDados {
+    public static void main(String[] args){
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21010121;
+        long cpf = 79527125L;
+        float pi = 3.14F;
+        double salario = 1320.33;
+    }
+}
