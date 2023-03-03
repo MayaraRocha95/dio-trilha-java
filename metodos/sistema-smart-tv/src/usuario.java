f8019b71dfbadde641376a940adb1469de859354
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-03-03 13:38:04 -0300

    sistema smartTv

--- metodos/sistema-smart-tv/src/usuario.java
+++ metodos/sistema-smart-tv/src/usuario.java
@@ -1,10 +1,22 @@
 public class usuario {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         
         SmartTv smartTv =  new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
 
         System.out.println("Tv ligada ? " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo status: Tv ligada?" + smartTv.ligada);
+
+        smartTv.desligar ();
+        System.out.println("Novo status: Tv ligada?" + smartTv.ligada);
     }
+
 }
